730bc658054f92b5fc2fe0242b85edcb521807d3
NickRayman <kolya.chechnev@mail.ru>
2023-05-09 18:37:26 +0300

    Создал проект VK_Project

---
+++ VK_Project/src/main/java/com/example/vk_project/VkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.vk_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkProjectApplication.class, args);
+	}
+
+}
